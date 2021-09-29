8282447bf165745227433f6175ac3339e86047a4
Atsushi <atsushi@moongift.jp>
2021-09-29 10:50:55 +0900

    Change initialize timing

--- todoapp/todoappApp.swift
+++ todoapp/todoappApp.swift
@@ -9,21 +9,13 @@ import SwiftUI
 
 @main
 struct todoappApp: App {
-    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
-        .onChange(of: scenePhase) { scene in
-            switch scene {
-            case .active:
+            VStack {
+                ContentView()
+            }.onAppear() {
                 // NCMBの初期化処理をここに書きます
                 print("これは不要です。削除してください")
-            case .inactive:
-                print("scenePhase: inactive")
-            case .background:
-                print("scenePhase: background")
-            @unknown default: break
             }
         }
     }
